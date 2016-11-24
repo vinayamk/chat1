240e232555a953c347290f8bef139e7948079cdb
Vinayak Malladad <vmalladad@cpapi-sandbox-ukapi-m03.ihost.aol.com>
2016-11-24 04:50:59 -0500

    ss

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+'use strict'
 
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
 
-console.log("ss");
+app.set('port', (process.env.PORT || 5000))
 
-var express = require("express");
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
 
-var app  = express();
-app.get('/',function(req,res){
-	res.send("ssssssss");
-	
-});
-app.listen(3000,function(req,res){
-	console.log("ss1");
-	
-});
+// parse application/json
+app.use(bodyParser.json())
+
+// index
+app.get('/', function (req, res) {
+	res.send('hello world i am a secret bot')
+})
+
+// for facebook verification
+
+// spin spin sugar
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
