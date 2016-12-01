2288ac93207618a222d147de8684e2f8697cbdb1
vinayamk <vinayak1988m@gmail.com>
2016-12-01 11:33:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -41,8 +41,26 @@ $jsonData = '{
     "recipient":{
         "id":"'.$sender.'"
     },
-    "message":{
-        "text":"'.$message_to_reply.'"
+     "message":{
+       "attachment":{
+			      "type":"template",
+			      "payload":{
+			        "template_type":"button",
+			        "text":"What do you want to do next?",
+			        "buttons":[
+			          {
+			            "type":"web_url",
+			            "url":"https://petersapparel.parseapp.com",
+			            "title":"Show Website"
+			          },
+			          {
+			            "type":"postback",
+			            "title":"Start Chatting",
+			            "payload":"USER_DEFINED_PAYLOAD"
+			          }
+			        ]
+			      }
+			    }
     }
 }';
 //Encode the array into JSON.
